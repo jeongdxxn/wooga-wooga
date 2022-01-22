1946c88cacc8241f2efed339409e80c5a60ee505
ssankq <ssankq@gmail.com>
2022-01-22 16:19:03 +0900

    API 요청 후 결과 데이터 표시

--- src/App.js
+++ src/App.js
@@ -1,39 +1,42 @@
 import './App.css'
 import axios from 'axios';
 import RegionAndTemp from './components/RegionAndTemp';
-import Forecast from './components/Forecast';
 import { useEffect, useState } from 'react';
-
 
 
 function App() {
 
   const [curWeather, setCurWeather] = useState('')
-  const api_keys = 'cd5f6cc939ea3acecedbae7014429728'
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=daegu&appid=${api_keys}`
-  const getWeather = (url) => {
-    return axios.get(url)
-      .then(res => setCurWeather(res.data))
+  const api_keys = '43a5e2520a415d989dd6aeb84fccc6c0'
+  const getWeather = async () => {
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=daegu&appid=${api_keys}`
+    return await axios.get(url)
   }
 
-  useEffect(async () => {
-    const weatherData = await getWeather(url)
-    // setCurWeather(weatherData)
-    // console.log(curWeather)
+  useEffect(() => {
+    getWeather()
+      .then((data) => {
+        setCurWeather(data)
+      })
+      .catch(err => console.log(err))
   }, [])
 
-  return (
-    <div className='App-background'>
-      <header className='App-header'>
-        Wooga Weather
-      </header>
-      <hr />
-      <section>
-        <RegionAndTemp curWeather={curWeather} />
-        <Forecast curWeather={curWeather} />
-      </section>
-    </div>
-  );
+  if (curWeather === '') return <div> 날씨를 불러오는 중입니다... </div>
+  else {
+    const propsData = curWeather.data
+
+      return(
+        <div className='App-background'>
+          <header className='App-header'>
+            Wooga Weather
+          </header>
+          <hr />
+          <section>
+             <RegionAndTemp propsData={propsData} />
+          </section>
+        </div>
+      );
+  }
 }
 
 export default App;
--- src/components/RegionAndTemp.js
+++ src/components/RegionAndTemp.js
@@ -1,24 +1,54 @@
 import React from "react";
 import './RegionAndTemp.css';
 
-function RegionAndTemp({ curWeather }) {
+function RegionAndTemp({ propsData }) {
 
-    // const temp = Math.floor(curWeather.main.temp - 273.15)
-    // console.log(curWeather.main.temp)
+    console.log(propsData)
+    const temp = Math.floor(propsData.main.temp - 273.15)
+    const city = propsData.name
+    const { humidity } = propsData.main
+    const { speed } = propsData.wind
+    const howsTheWeather = (temp) => {
+        let weatherStr = ''
+        if(temp < 5) weatherStr = `It's getting Cold`
+        else if (5 <= temp && temp < 25) weatherStr = `It's Good day`
+        else if (25 <= temp) weatherStr = `It's getting Hot`
+
+        if(propsData.rain) weatherStr = `It's going to Rainy`
+        if(propsData.snow) weatherStr = `It's going to Snow`
+
+        return weatherStr;
+    } 
 
     return (
-        <div className="Main-content">
-            <span>
-                <div className="Region">
-                    Daegu
-                </div>
-                <div className="Condition">
-                    It's getting Cold
-                </div>
-            </span>
-            <span className="Temperature">
-                temp°
-            </span>
+        <div>
+            <div className="Main-content-RT">
+                <span>
+                    <div className="Region">
+                        {city}
+                    </div>
+                    <div className="Condition">
+                        {howsTheWeather(temp)}
+                    </div>
+                </span>
+                <span className="Temperature">
+                    {temp}°
+                </span>
+            </div>
+            <div className="Main-content-F">
+                <span>
+                    <div>습도</div>
+                    <div>{humidity}%</div>
+                </span>
+                <span>
+                    <div>풍속</div>
+                    <div>{speed}m/s</div>
+                </span>
+                <span>
+                    <div>미세먼지</div>
+                    <div>보통</div>
+                </span>
+            </div>
         </div>
     )
 }
